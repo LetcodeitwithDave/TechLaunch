e5ef292eaf64ea6f50ea2210b49b34a55c89bbc3
Dave <sololadavid4@gmail.com>
2024-05-30 20:17:40 +0100

    properly configured the api call

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,29 +1,58 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { RAPID_API_KEY } from '@env'
+import { useInputContext } from "../authcontext/inputContext";
 
-const rapidApiKey = RAPID_API_KEY;
 
-const useFetch = (endpoint) => {
+
+const useFetch = () => {
+    const { searchInput } = useInputContext()
+
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
 
 
     const options = {
         method: 'GET',
         // make the endpoint dynamic
-        url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+        url: 'https://jsearch.p.rapidapi.com/search',
         headers: {
-            'X-RapidAPI-Key': rapidApiKey,
+            'X-RapidAPI-Key': '8ea29b4f1amshb945df4cfa16601p18b0b1jsnd5b1db74b6f4',
             'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
         },
         params: {
-            query: 'Python developer in Texas, USA',
-            page: '1',
+            query: searchInput || 'Django Developer',
+            // query: `${searchInput}`,
+
             num_pages: '1'
         },
     };
 
+
     // fetch the data
-    const fetchData = () => {
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.request(options)
+            console.log(response.data.data)
+            setData(response.data.data);
+
+        } catch (error) {
+            setError(Error);
+            console.log('there is an error somewhere');
+        } finally {
+            setIsLoading(false);
+        }
 
     }
+    useEffect(() => {
+        fetchData();
+    }, [searchInput])
+
+    return { data, error, isLoading }
 }
+
+export default useFetch;
+// fetchData is consuming option,
+// useEffect is comsuming fetchData
