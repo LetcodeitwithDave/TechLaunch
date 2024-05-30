17507efd8711361400c3d9336824585a611d071b
Dave <sololadavid4@gmail.com>
2024-05-30 20:11:16 +0100

    index

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import Nav from "./Nav";
+import Input from './Input'
+import JobCard from "./JobCard";
+import JobDetail from "./JobDetail";
+
+export {
+    Nav,
+    Input,
+    JobCard,
+    JobDetail,
+}
