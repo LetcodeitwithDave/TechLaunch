ee8962fadca42a762f6a7589b7230c09c63a71d2
Dave <sololadavid4@gmail.com>
2024-06-06 04:15:36 +0100

    updated the design a bit

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -29,7 +29,7 @@ function Nav() {
                   className="px-0  h-20 w-[120px] md:h-[90px] md:w-[120px] bg-transparent"
                   
                 /> */}
-                <h1 className=' text-white font-bold text-2xl font-montserrat '>
+                <h1 className=' px-2 text-white font-bold text-2xl font-montserrat '>
                   Dev<span className=' text-green-400'><br></br>Board</span>
                 </h1>
               </div>
@@ -65,39 +65,41 @@ function Nav() {
 
               {isSideNavOpen ? (
                 
-                <button className="text-black h-[25px] text-4xl sm:hidden 0" onClick={toggleSideNav}>
+                <button className=" text-red-700 h-[25px] text-4xl sm:hidden 0" onClick={toggleSideNav}>
                   <IonIcon icon={close} />
                 </button>
               ) : (
-                <button className="text-white h-[25] text-4xl sm:hidden" onClick={toggleSideNav}>
+                <button className=" focus:ring-2 focus:out focus:outline-none  focus:ring-green-700 rounded text-white h-[25] text-4xl sm:hidden" onClick={toggleSideNav}>
                   <IonIcon icon={menuOutline} />
                 </button>
               )}
              
             
             </div>
-                { 
-                  isSideNavOpen &&
-                  (
-                  <div>
-                      <button className="text-black h-[25px] text-4xl sm:hidden 0" onClick={toggleSideNav}>
+            { 
+              isSideNavOpen &&
+              (
+              <div>
+                
+                  <div className=" absolute sm:hidden right-0 top-0 h-screen w-2/3 bg-green-800 text-white z-10 py-6 px-2 transform translate-x-0 transition-transform duration-300">
+                      <button className=" focus:ring-2 focus:out focus:outline-none  focus:ring-green-700 rounded transition duration-200 my-2 text-white h-[33px] text-4xl sm:hidden mb-2" onClick={toggleSideNav}>
                         <IonIcon icon={close} />
                       </button>
-                      <div className=" absolute sm:hidden top-0 right-0 h-screen w-2/3 bg-green-800 text-white z-10 py-6 px-2 transform translate-x-0 transition-transform duration-300">
-                          {navLinks.map((item)=>(
-                          
-                            <a 
-                            href={item.href} key={item.label} >
-                                <div className=" py-3 px-4 hover:bg-green-700 rounded transition duration-200">
-                                  {item.label}
-
-                                </div>
-                              </a>
-                            
-                            ))}
-                      </div>
                       
+                      {navLinks.map((item)=>(
+                      
+                        <a 
+                        href={item.href} key={item.label} >
+                            <div className=" py-3 px-4 hover:bg-green-700 rounded transition duration-200">
+                              {item.label}
+
+                            </div>
+                          </a>
+                        
+                        ))}
                   </div>
+                  
+              </div>
             )}
             
         </nav>
