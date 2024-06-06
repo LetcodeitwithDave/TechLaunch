8a02b01cc7a794ce5c0eb0471cdc9ebf3fadc4a8
Dave <sololadavid4@gmail.com>
2024-06-06 04:08:11 +0100

    index js

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,6 +1,10 @@
 import Header from "./Header";
 import Jobsection from "./Jobsection";
+import Main from "./Main";
+// import Main
 export {
     Header,
-    Jobsection
+    Jobsection,
+    Main
+
 }
