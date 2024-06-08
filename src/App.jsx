3d2b7813079a6cde18f2524decf18d494e90df56
Dave <sololadavid4@gmail.com>
2024-06-08 22:48:47 +0100

    created route for the landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Main } from './sections';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import { LandingHeader } from './landingpage/components';
+import LandingPage from './landingpage/LandingPage';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <Router>
         <Routes>
           <Route   path='/home' element={<Main />} />
-          <Route   path='/' element={<LandingHeader />} />
+          <Route   path='/' element={<LandingPage />} />
         </Routes>
       </Router>
 
