06a1d2c35845b60567a0f4da86d1f71ec491d17a
Dave <sololadavid4@gmail.com>
2024-06-10 21:21:04 +0100

    added more components

--- src/landingpage/components/index.js
+++ src/landingpage/components/index.js
@@ -1,7 +1,12 @@
 import LandingHeader from "./LandingHeader";
 import LandingHero from "./LandingHero";
+import OfferSection from './OfferSection'
+import FeaturedJobs from "./FeaturedJobs";
+
 
 export {
     LandingHeader,
-    LandingHero
+    LandingHero,
+    OfferSection,
+    FeaturedJobs
 }
