9e5a3661c559e30ce470a1099a54515f1b63bfb3
Dave <sololadavid4@gmail.com>
2024-06-10 21:19:40 +0100

    added more section to the landing page

--- src/landingpage/LandingPage.jsx
+++ src/landingpage/LandingPage.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { LandingHeader, LandingHero } from "./components";
+import { LandingHeader, LandingHero, OfferSection, FeaturedJobs } from "./components";
 
 function LandingPage() {
   return (
-    <main className=" bg-pageBackground">
+    <main className=" ">
       <section className=" bg-white">
         <LandingHeader />
       </section>
-      <section className=" bg-heroBackground ">
+      <section className=" bg-heroBackground">
         <LandingHero />
+      </section>
+
+      <section  className="mt-11 mb-14">
+        <OfferSection />
+      </section>
+
+      <section  className="mt-11 mb-14">
+        <FeaturedJobs />
       </section>
     </main>
   );
