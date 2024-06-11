89c5cc8f9d18209195ac7ebf59e1ca7af98e1b02
Dave <sololadavid4@gmail.com>
2024-06-11 13:03:10 +0100

    created the structure, still gonna update it

---
+++ src/landingpage/components/FeaturedJobs.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { companyLogo1, companyLogo2, companyLogo3 } from '../../assets/landingpageimage'
+
+function FeaturedJobs() {
+  return (
+    <div>
+        <h1 className=' flex justify-center py-9  text-4xl font-montserratBold text-textColor mb-16'> 
+            We Offer Best Services
+        </h1>
+        <div  className='  px-4 flex justify-evenly'>
+            <div className=' relative'>
+                <img 
+                    src={companyLogo1}
+                    className=' w-[92px] h-[93px] absolute -top-14 left-10'
+                />
+                <div className=' border  border-gray-300 py-14  px-10 bg-white rounded-xl w-80 h-72'>
+                    
+                    <h1 className=' font-montserratBold text-textColor text-2xl'>Remote Work</h1>
+
+                    <p className=' font-montserratRegular py-5 text-subheading text-lg'>
+                    Discover remote work opportunities that allow you to gain experience without compromising your studies.
+                    </p>
+                </div>
+            </div>
+
+            <div className=' relative'>
+                <img 
+                    src={companyLogo2}
+                    className=' w-[92px] h-[93px] absolute -top-14 left-10'
+                />
+                <div className=' border border-gray-300 py-14  px-10 bg-white rounded-xl w-80 h-72'>
+                    
+                    <h1 className=' font-montserratBold text-textColor text-2xl'>Intership Opportunities</h1>
+
+                    <p className=' font-montserratRegular py-5 text-subheading text-lg'>
+                    We connect you with tech companies offering internships that fit your schedule and career goals
+                    </p>
+                </div>
+            </div>
+
+            <div className=' relative'>
+                <img 
+                    src={companyLogo3}
+                    className=' w-[92px] h-[93px] absolute -top-14 left-10'
+                />
+                <div className=' border border-gray-300 py-14  px-10 bg-white rounded-xl w-80 h-72'>
+                    
+                    <h1 className=' font-montserratBold text-textColor text-2xl'>Part-Time Jobs</h1>
+
+                    <p className=' font-montserratRegular py-5 text-subheading text-lg'>
+                    Find part-time job opportunities that fit your student schedule, allowing you to earn while you learn
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default FeaturedJobs
