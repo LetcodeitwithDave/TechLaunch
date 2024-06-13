83157e14421b47e5164f03bacb45428818ecb86a
Dave <sololadavid4@gmail.com>
2024-06-13 20:36:53 +0100

    images for HOW IT WORKS section

---
+++ src/assets/landingpageimage/howitworksimages/index.js
@@ -0,0 +1,19 @@
+import search1 from './search1.png'
+import apply2 from './apply2.png'
+import getHired1 from './getHired1.png'
+import search2 from './search2.png'
+import apply1 from './apply1.png'
+import getHired4 from './getHired4.png'
+import howItWorkImage from './howitworks1.jpg'
+
+
+
+export{
+    search1,
+    apply2,
+    getHired1,
+    search2,
+    apply1,
+    getHired4,
+    howItWorkImage
+}
