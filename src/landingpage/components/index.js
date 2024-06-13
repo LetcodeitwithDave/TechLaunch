7bf267691dfe54a26b095501924a802beba54e78
Dave <sololadavid4@gmail.com>
2024-06-13 20:41:09 +0100

    added HowItWorks.jsx component

--- src/landingpage/components/index.js
+++ src/landingpage/components/index.js
@@ -2,11 +2,13 @@ import LandingHeader from "./LandingHeader";
 import LandingHero from "./LandingHero";
 import OfferSection from './OfferSection'
 import FeaturedJobs from "./FeaturedJobs";
+import HowItWorks from "./HowItWorks";
 
 
 export {
     LandingHeader,
     LandingHero,
     OfferSection,
-    FeaturedJobs
+    FeaturedJobs,
+    HowItWorks
 }
