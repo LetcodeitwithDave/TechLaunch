c6e4c409fe24cb961e5bfad9de32df524e0cae58
Dave <sololadavid4@gmail.com>
2024-06-13 20:38:47 +0100

    .

--- src/assets/landingpageimage/index.js
+++ src/assets/landingpageimage/index.js
@@ -7,9 +7,6 @@ import remoteImgage from './global-network.png'
 import companyLogo1 from './companyLogo1.png'
 import companyLogo2 from './companyLogo2.jpg'
 import companyLogo3 from './companyLogo3.png'
-import search1 from './howitworksimages/search1.png'
-import apply2 from './howitworksimages/apply2.png'
-import getHired1 from './howitworksimages/getHired1.png'
 
 
 
@@ -23,9 +20,6 @@ export {
     companyLogo1,
     companyLogo2,
     companyLogo3,
-    search1,
-    apply2,
-    getHired1,
     
 }
 
