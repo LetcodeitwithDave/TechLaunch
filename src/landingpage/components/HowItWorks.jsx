6c3184d095df73a04b68b1c282714078defd8a6e
Dave <sololadavid4@gmail.com>
2024-06-14 21:45:37 +0100

    fixed the reponsiveness across screens

--- src/landingpage/components/HowItWorks.jsx
+++ src/landingpage/components/HowItWorks.jsx
@@ -12,55 +12,57 @@ import {
 
 function HowItWorks() {
   return (
-    <div className="px-8 md:px-16 lg:px-32">
-      <div className="flex flex-col md:flex-row">
-        <div className="pl-0">
+    <div className="px-4 md:px-16 lg:px-32">
+      <div className="flex flex-col md:flex-row md:items-start">
+        <div className="pl-0 md:w-1/2 lg:w-2/5">
           <div className="pb-6">
             <p className="font-montserratRegular text-textColor text-sm">easy and fast</p>
-            <h1 className="font-montserratSemibold text-3xl w-full md:text-4xl md:w-64 lg:text-5xl lg:w-80 pt-2">
+            <h1 className="font-montserratSemibold text-3xl md:text-4xl lg:text-5xl pt-2">
               Get Hired In 3 Easy Steps
             </h1>
           </div>
 
-          <div className="pl-0 ml-0 ">
-            <div className="pb-9 flex items-start gap-4 md:gap-7">
+          <div className="pl-0 ml-0">
+            <div className="pb-6 md:pb-9 flex items-start gap-4 md:gap-7">
               <img src={search1} alt="Search Icon" className="h-12 w-12 md:h-14 md:w-14" />
               <div className="">
                 <h2 className="font-montserratSemibold text-lg">Search</h2>
-                <p className="font-montserratRegular text-subheading leading-relaxed  w-72">
+                <p className="font-montserratRegular text-subheading leading-relaxed">
                   Enter your desired job title and location.
                 </p>
               </div>
             </div>
-            <div className="pb-9 flex items-start gap-4 md:gap-7">
+            <div className="pb-6 md:pb-9 flex items-start gap-4 md:gap-7">
               <img src={apply2} alt="Apply Icon" className="h-12 w-12 md:h-14 md:w-14" />
               <div className="max-w-sm">
                 <h2 className="font-montserratSemibold text-lg">Apply</h2>
-                <p className="font-montserratRegular text-subheading leading-relaxed w-72">
+                <p className="font-montserratRegular text-subheading leading-relaxed">
                   Select the job that fits your needs and submit your application.
                 </p>
               </div>
             </div>
-            <div className="pb-9 flex items-start gap-4 md:gap-7">
+            <div className="pb-6 md:pb-9 flex items-start gap-4 md:gap-7">
               <img src={getHired3} alt="Get Hired Icon" className="h-12 w-12 md:h-14 md:w-14" />
               <div className="max-w-sm">
                 <h2 className="font-montserratSemibold text-lg">Get Hired</h2>
-                <p className="font-montserratRegular text-subheading leading-relaxed  w-72">
+                <p className="font-montserratRegular text-subheading leading-relaxed">
                   Connect with employers and start your new role in tech.
                 </p>
               </div>
             </div>
           </div>
         </div>
-        {/* w-auto h-auto */}
-        <div className=" mt-4 md:mt-20 md:ml-32 lg:mt-0 ">
-          <img src={howItWorkImage}  alt="How it works" className=" h-auto w-auto  flex-shrink-0 rounded-lg "/>
-        </div> 
+
+        <div className="mt-4 md:mt-0 md:ml-10 lg:ml-20 md:w-1/2 lg:w-3/5">
+          <img
+            src={howItWorkImage}
+            alt="How it works"
+            className="h-auto w-full flex-shrink-0 rounded-lg"
+          />
+        </div>
       </div>
     </div>
   );
 }
-//             {/* pb-9 md:pb-5 lg:pb-9    h-12 w-12 md:h-10 md:w-10 lg:h-12 lg:w-12   */}
-
 
 export default HowItWorks;
