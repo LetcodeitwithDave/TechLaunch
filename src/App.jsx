3f396d2987e0d5aefbe2a9ee69d97057b9b7b57d
Dave <sololadavid4@gmail.com>
2024-06-22 14:45:10 +0100

    added route to Login.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import React from "react";
 import { Main } from "./sections";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./landingpage/LandingPage";
-import { Signup } from "./components";
+import { Signup, Login } from "./components";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { far } from '@fortawesome/free-regular-svg-icons'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { far } from "@fortawesome/free-regular-svg-icons";
 
 // // import the library
 // import { library } from '@fortawesome/fontawesome-svg-core'
 
 // // import your icons
 // import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fas } from "@fortawesome/free-solid-svg-icons";
 // import { far } from '@fortawesome/free-regular-svg-icons'
 
 function App() {
@@ -24,6 +24,7 @@ function App() {
           <Route path="/home" element={<Main />} />
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
     </div>
@@ -31,4 +32,4 @@ function App() {
 }
 
 export default App;
-library.add( far, fas)
+library.add(far, fas);
