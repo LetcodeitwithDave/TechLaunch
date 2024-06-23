712a4bc74b1590a399c305cbb87bd02dc6a4126b
Dave <sololadavid4@gmail.com>
2024-06-23 21:13:56 +0100

    .

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -14,7 +14,7 @@ def signup( request):
         serializer.save()
         return Response ({'msg':'User created successfully'}, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
