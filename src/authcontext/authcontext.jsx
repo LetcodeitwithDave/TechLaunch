3eceab46e4acdfde6d6f008254a9fb98f81055bf
Dave <sololadavid4@gmail.com>
2024-06-25 23:12:52 +0100

    wrote conditionals for checking if token is available

--- src/authcontext/authcontext.jsx
+++ src/authcontext/authcontext.jsx
@@ -1,28 +1,26 @@
 // inputContext.js
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext, useEffect } from "react";
 
 const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
-export const AuthProvider = ({ children }) =>  {
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
+  // get access_token if available
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, []);
 
-    // get access_token if available
-    useEffect(() => {
-        const token = localStorage.getItem('access_token')
-        if (token){
-            setIsAuthenticated(true)
-        }
-
-    }, []);
-
-
-    return (
-        <AuthContext.Provider value={{isAuthenticated, setIsAuthenticated}} >
-            {children}
-        </AuthContext.Provider>
-    );
-}
-
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
