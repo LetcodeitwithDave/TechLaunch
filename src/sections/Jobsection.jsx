91dacadc151e6b3b51f574bd806e20bd9db8fbfa
Dave <sololadavid4@gmail.com>
2024-06-27 12:59:19 +0100

    changed something

--- src/sections/Jobsection.jsx
+++ src/sections/Jobsection.jsx
@@ -3,14 +3,23 @@ import { JobCard, JobDetail } from "../components";
 
 function Jobsection() {
   return (
-    <div className=" flex  max-container justify-center  ">
-      <div className=" flex  flex-col pr-6 ">
+    <div className=" flex  justify-center  ">
+      <div className="flex  flex-col mx-3 lg:mx-0 md:pr-6 ">
         <JobCard />
       </div>
-      <div className=" lg:w-1/3 h-screen overflow-y-auto sticky top-0">
+      <div className="hidden md:block lg:w-1/3 h-screen overflow-y-auto sticky top-0">
         <JobDetail />
       </div>
     </div>
+
+    // <div className=" flex justify-center  ">
+    //   <div className=" w-full flex  flex-col mx-3 ">
+    //     <JobCard />
+    //   </div>
+    //   <div className="hidden md:block h-screen overflow-y-auto sticky top-0">
+    //     <JobDetail />
+    //   </div>
+    // </div>
   );
 }
 
