d95a41ffc3a141a1bb2b4f7aef8a3d12346b18ae
Dave <sololadavid4@gmail.com>
2024-06-28 13:22:40 +0100

    adjusted how data is beign rendered

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 // InputComponent.js
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { InputContext } from "../authcontext/inputContext";
 
 function Input() {
@@ -16,12 +16,13 @@ function Input() {
   const options = {
     method: "GET",
     headers: {
-      "x-rapidapi-key": "8ea29b4f1amshb945df4cfa16601p18b0b1jsnd5b1db74b6f4",
+      // 86363862bbmsh9eb0d6ad11b97e8p1bec71jsn7419c6a18d1b
+      "x-rapidapi-key": "86363862bbmsh9eb0d6ad11b97e8p1bec71jsn7419c6a18d1b",
       "x-rapidapi-host": "jsearch.p.rapidapi.com",
     },
   };
 
-  const fetchData = async (searchInput) => {
+  const fetchData = async () => {
     try {
       // const response = await fetch(url, options);
       const response = await fetch(
@@ -30,13 +31,25 @@ function Input() {
         }&page=1&num_pages=1&date_posted=all&remote_jobs_only=true`,
         options
       );
+      console.log(response);
       const result = await response.json();
+      console.log(result.data);
 
       // store api in state
       setUserData(result.data);
-      console.log(result.data);
+      // console.log(result.data);
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      fetchData(searchInput || "Django Developer");
     }
   };
 
@@ -49,12 +62,13 @@ function Input() {
           name="searchInput"
           value={searchInput}
           onChange={(e) => setSearchInput(e.target.value)}
+          onKeyDown={handleKeyPress}
           className="shadow-lg rounded-md p-3 pr-12 w-full h-14 focus:out focus:outline-none focus:ring-2 focus:ring-blue-500"
           placeholder="Job title, keywords, or company"
         />
         <button
           type="submit"
-          onClick={() => fetchData(searchInput)}
+          onClick={() => fetchData()}
           className="absolute right-3 top-3"
         >
           <svg
