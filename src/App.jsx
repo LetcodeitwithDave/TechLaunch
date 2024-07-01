ebf250f3c310c3535084dc9066e9f61338703485
Dave <sololadavid4@gmail.com>
2024-07-01 04:01:05 +0100

    fix page rendering for auth users

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
 // App.js
 import React from "react";
-import { Main } from "./sections";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import LandingPage from "./landingpage/LandingPage";
 import { Signup, Login } from "./components";
-
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { far } from "@fortawesome/free-regular-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
-import { AuthProvider } from "./authcontext/authcontext";
+import { AuthProvider, useAuth } from "./authcontext/authcontext";
 import ProtectedRoute from "./authcontext/ProtectedRoute";
+import { Main } from "./sections";
 function App() {
+  const isAuthenticated = localStorage.getItem("access_token");
   return (
     <AuthProvider>
       <Router>
         <Routes>
           <Route path="/home" element={<ProtectedRoute />} />
-          <Route path="/" element={<LandingPage />} />
+          <Route
+            path="/"
+            element={
+              isAuthenticated ? <Navigate to="/home" /> : <LandingPage />
+            }
+          />
           <Route path="/signup" element={<Signup />} />
           <Route path="/login" element={<Login />} />
         </Routes>
