1f18f5eb4b830671efa3a2551341ebf60b605526
Dave <sololadavid4@gmail.com>
2024-07-07 20:16:35 +0100

    adjustd UI

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -16,34 +16,58 @@ const Dropdown = ({ options, title, onFilterSelect, displayOptions }) => {
     console.log("this is the value -> ", value);
   };
 
+  const selectedOption = () => {
+    setSelectedValue(false);
+  };
+
   return (
     <div className=" relative">
       {/* Dropdown Button */}
-      <div className="text-textColor flex bg-gray-200 font-montserratRegular focus:ring-gray-400 hover:bg-gray-300 focus:ring-1 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center items-center">
-        <button
-          id="dropdownDefaultButton"
-          onClick={toggleDropdown}
-          type="button"
-          className=""
-        >
-          {selectedValue ? selectedValue : title}
-        </button>
-        <svg
-          className="w-2.5 h-2.5 ms-3"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 10 6"
-          fill="none"
-          aria-hidden="true"
-        >
-          <path
+      <button
+        id="dropdownDefaultButton"
+        onClick={toggleDropdown}
+        type="button"
+        className="text-textColor inline-flex bg-gray-200 font-montserratRegular focus:ring-gray-400 hover:bg-gray-300 focus:ring-1 focus:outline-none  font-medium rounded-lg px-5 py-2.5 text-center text-sm items-center"
+      >
+        {selectedValue ? (
+          <div onClick={selectedOption}>{selectedValue}</div>
+        ) : (
+          title
+        )}
+        {selectedValue ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
             stroke="currentColor"
+            strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            strokeWidth="2"
-            d="m1 1 4 4 4-4"
-          />
-        </svg>
-      </div>
+            className="lucide lucide-x w-4 ms-2 h-4"
+          >
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+          </svg>
+        ) : (
+          <svg
+            className="w-2.5 h-2.5 ms-3"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 10 6"
+            fill="none"
+            aria-hidden="true"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="m1 1 4 4 4-4"
+            />
+          </svg>
+        )}
+      </button>
 
       {/* Dropdown Menu */}
       {isOpen ? (
