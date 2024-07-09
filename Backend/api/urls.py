685c32d8bcd07a959f061bc780e3b187746ac9bb
Dave <sololadavid4@gmail.com>
2024-07-09 22:59:37 +0100

    path to account view

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup, name='sign up rout'),
     path('login/', views.login, name='login'),
+    path('account', views.account,  name='account'),
     path('protectedview/', views.protectedView, name='protect'),
 ]
