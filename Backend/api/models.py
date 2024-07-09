97b6b48ababe4b234ea78a6097da439c373ef456
Dave <sololadavid4@gmail.com>
2024-07-09 22:58:36 +0100

    dummy data

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -47,8 +47,11 @@ class MyUser(AbstractBaseUser, PermissionsMixin):
         return f"email= {self.email}  - firstname = {self.firstname} - lastname = {self.lastname} "
 
 
-class Product(models.Model):
+class Account(models.Model):
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=300)
+
+    def __str__(self) :
+        return f'firstname = {self.firstname}, lastname = {self.lastname}, email= {self.email}'
