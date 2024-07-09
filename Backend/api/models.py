a35075374ea674ab1bad6c6a03f7570e7f8c68f6
Dave <sololadavid4@gmail.com>
2024-07-09 21:10:42 +0100

    fix issue with auth user & included firstname , lastname for auth

--- Backend/api/models.py
+++ Backend/api/models.py
@@ -1,23 +1,35 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class MyUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, firstname, lastname , password=None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
+        
+        if not firstname:
+            raise ValueError('The Firstname field must be set')
+        
+        if not lastname:
+            raise ValueError('The Lastname field must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=email, firstname=firstname, lastname=lastname, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email,firstname, lastname, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
 
-class MyUser(AbstractBaseUser):
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, firstname, lastname, password, **extra_fields)
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     firstname = models.CharField(max_length=50)
     lastname = models.CharField(max_length=50)
@@ -33,3 +45,10 @@ class MyUser(AbstractBaseUser):
 
     def __str__(self):
         return f"email= {self.email}  - firstname = {self.firstname} - lastname = {self.lastname} "
+
+
+class Product(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=300)
