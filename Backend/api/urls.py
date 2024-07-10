71a9d93b90919b444d5c56bdfa91a3b4fde3b24c
Dave <sololadavid4@gmail.com>
2024-07-10 21:22:14 +0100

    add api to route

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+
 
 
 urlpatterns = [
@@ -7,4 +9,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('account', views.account,  name='account'),
     path('protectedview/', views.protectedView, name='protect'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token refresh'),
+    path('token/access', TokenObtainPairView.as_view(), name='token access')
+
 ]
