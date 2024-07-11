f3e4437975fbf67717540525e4970ee29e353ad6
Dave <sololadavid4@gmail.com>
2024-07-11 01:20:29 +0100

    refactor code for route

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,51 @@ import { AuthProvider, useAuth } from "./authcontext/authcontext";
 import ProtectedRoute from "./authcontext/ProtectedRoute";
 import { Main } from "./sections";
 import Account from "../../Account";
+import Know from "../public/Know";
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <AppRoutes />
+        <Routes>
+          <Route element={<Account />} path="/test" />
+          <Route element={<Login />} path="/login" />
+
+          <Route element={<Signup />} path="/signup" />
+          <Route
+            element={<ProtectedRoute children={<Know />} />}
+            path="/know"
+          />
+        </Routes>
       </Router>
     </AuthProvider>
     // </div>
   );
 }
 
-const AppRoutes = () => {
-  const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
-  return (
-    <Routes>
-      <Route path="/home" element={<ProtectedRoute />} />
-      <Route
-        path="/"
-        element={isAuthenticated ? navigate("/home") : <LandingPage />}
-      />
-      <Route
-        path="/signup"
-        element={isAuthenticated ? navigate("/home") : <Signup />}
-      />
-      <Route
-        element={isAuthenticated ? navigate("/home") : <Login />}
-        path="/login"
-      />
+// const AppRoutes = () => {
+//   const navigate = useNavigate();
+//   const { isAuthenticated } = useAuth();
+//   return (
+//     <Routes>
+//       <Route path="/home" element={<ProtectedRoute />} />
+//       <Route
+//         path="/"
+//         element={isAuthenticated ? navigate("/home") : <LandingPage />}
+//       />
+//       <Route
+//         path="/signup"
+//         element={isAuthenticated ? navigate("/home") : <Signup />}
+//       />
+//       <Route
+//         element={isAuthenticated ? navigate("/home") : <Login />}
+//         path="/login"
+//       />
 
-      <Route path="/test" element={<Account />} />
-    </Routes>
-  );
-};
+//       <Route path="/test" element={<Account />} />
+//     </Routes>
+//   );
+// };
 
 export default App;
 library.add(far, fas);
