6dd9f4a7befe1b14eb4b72e3f4a8002b83019c40
Dave <sololadavid4@gmail.com>
2024-07-12 00:44:30 +0100

    update token logic for getting new token before token expire

--- src/authcontext/authcontext.jsx
+++ src/authcontext/authcontext.jsx
@@ -7,11 +7,61 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const data = localStorage.getItem("authtoken");
+  const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(
     localStorage.getItem("authtoken")
       ? JSON.parse(localStorage.getItem("authtoken"))
       : null
   );
+
+  // console.log("refresh token in localstorage", isAuthenticated.refresh);
+
+  const logout = () => {
+    setIsAuthenticated(null);
+    localStorage.removeItem("authtoken");
+    console.log("logged out");
+  };
+
+  const updateToken = async () => {
+    try {
+      console.log("update token");
+      const response = await fetch("http://localhost:8000/api/token/refresh", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refresh: isAuthenticated?.refresh }),
+      });
+      console.log("response -> ", response);
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("token from update token -> ", data);
+        localStorage.setItem("authtoken", JSON.stringify(data));
+        setIsLoading(false);
+        setIsAuthenticated(data);
+      } else {
+        logout();
+      }
+      if (isLoading) {
+        setIsLoading(false);
+      }
+    } catch (error) {
+      console.log("something went wrong");
+    }
+  };
+
+  useEffect(() => {
+    if (isLoading) {
+      updateToken();
+    }
+    const time = setInterval(() => {
+      if (isAuthenticated) {
+        updateToken();
+      }
+    }, 870000);
+    return () => clearInterval(time);
+  }, [isAuthenticated]);
 
   // // get access_token if available
   // useEffect(() => {
@@ -27,7 +77,7 @@ export const AuthProvider = ({ children }) => {
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
-      {children}
+      {isLoading ? null : children}
     </AuthContext.Provider>
   );
 };
