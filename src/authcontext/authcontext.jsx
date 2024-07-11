2b07477507dd1978e2b7b9e53473ed1167391fb1
Dave <sololadavid4@gmail.com>
2024-07-11 01:15:45 +0100

    refactor code for context api

--- src/authcontext/authcontext.jsx
+++ src/authcontext/authcontext.jsx
@@ -1,24 +1,29 @@
 // inputContext.js
 import React, { createContext, useState, useContext, useEffect } from "react";
 
-const AuthContext = createContext();
+export const AuthContext = createContext();
 
 export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const data = localStorage.getItem("authtoken");
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem("authtoken")
+      ? JSON.parse(localStorage.getItem("authtoken"))
+      : null
+  );
 
-  // get access_token if available
-  useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    console.log("isAuthenticated data in authcontext", token);
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-    console.log("isAuthenticated state: ", isAuthenticated);
-  }, []);
+  // // get access_token if available
+  // useEffect(() => {
+  //   const token = localStorage.getItem("access_token");
+  //   console.log("isAuthenticated data in authcontext", token);
+  //   if (token) {
+  //     setIsAuthenticated(true);
+  //   } else {
+  //     setIsAuthenticated(false);
+  //   }
+  //   console.log("isAuthenticated state: ", isAuthenticated);
+  // }, []);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
