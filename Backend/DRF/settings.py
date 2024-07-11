73909e02cb6a16e4e7c83954c65db4dfcedb144b
Dave <sololadavid4@gmail.com>
2024-07-12 00:46:28 +0100

    15 minute token lifetime

--- Backend/DRF/settings.py
+++ Backend/DRF/settings.py
@@ -36,7 +36,7 @@ REST_FRAMEWORK = {
 CORS_ALLOW_ALL_ORIGINS = True  # Allow requests from any origin
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
