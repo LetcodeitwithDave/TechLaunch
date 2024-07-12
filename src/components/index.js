65485de4cc716c3312e6da30a2754c47613a867c
Dave <sololadavid4@gmail.com>
2024-07-12 11:13:24 +0100

    move Login & Signup page to section folder

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,6 @@ import Nav from "./Nav";
 import Input from './Input'
 import JobCard from "./JobCard";
 import JobDetail from "./JobDetail";
-import Signup from './Signup.jsx'
-import Login from './Login.jsx'
 import Dropdown from "./Dropdown.jsx";
 import CompanyFilter from "./CompanyFilter.jsx";
 
@@ -12,8 +10,6 @@ export {
     Input,
     JobCard,
     JobDetail,
-    Signup,
-    Login,
     Dropdown,
     CompanyFilter
 
