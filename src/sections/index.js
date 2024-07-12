c456a10dd08366243dfa86373e459543f5d75458
Dave <sololadavid4@gmail.com>
2024-07-12 11:10:54 +0100

    move Login & Signup page to section folder

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,10 +1,14 @@
 import Header from "./Header";
 import Jobsection from "./Jobsection";
 import Main from "./Main";
+import Login from "./Login";
+import SignUp from "./Signup";
 // import Main
 export {
     Header,
     Jobsection,
-    Main
+    Main,
+    Login,
+    SignUp,
 
 }
