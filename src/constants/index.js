8c235545b219e61143fa1acf572bd181d58df3c9
Dave <sololadavid4@gmail.com>
2024-07-13 00:36:56 +0100

    index for const list

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+export const datePostedDisplay = [
+    { label: "All day" },
+    { label: "Past 24 hours" },
+    { label: "Past 3days" },
+    { label: "Past week" },
+    { label: "Past month" },
+  ];
+
+export const datePostedOption = ["all", "today", "3days", "week", "month"]
+
+export const jobTypeDisplay = [
+    { label: "Full-Time" },
+    { label: "Contract" },
+    { label: "Part-time" },
+    { label: " Intern" },
+  ];
+
+
+export const jobTypeOption=["FULLTIME", "CONTRACTOR", "PARTTIME", "INTERN"]
+
