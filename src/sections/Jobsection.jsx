f25d2a91e8b02928906ca4896c2a156dfcf8450c
Dave <sololadavid4@gmail.com>
2024-07-13 19:19:45 +0100

    adusted design

--- src/sections/Jobsection.jsx
+++ src/sections/Jobsection.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import { JobCard, JobDetail } from "../components";
+import { JobCard, JobDetail, Filter, ProfileCard } from "../components";
 
 function Jobsection() {
   return (
-    <div className=" flex  justify-center  ">
-      <div className="flex  flex-col mx-3 lg:mx-0 md:pr-6 ">
+    <div className=" flex  gap-6 ">
+      <div className=" justify-start hidden ml-6 h-screen lg:block overflow-y-auto sticky top-0">
+        <Filter />
+      </div>
+
+      <div className="flex mx-4 lg:mx-0 flex-col ">
         <JobCard />
       </div>
-      <div className="hidden md:block lg:w-1/3 h-screen overflow-y-auto sticky top-0">
-        <JobDetail />
+
+      <div className="justify-end hidden lg:block ml-auto mr-6   ">
+        <ProfileCard />
       </div>
+      {/* <div className="hidden md:block lg:w-1/3 h-screen overflow-y-auto sticky top-0">
+        <JobDetail />
+      </div> */}
     </div>
 
     // <div className=" flex justify-center  ">
