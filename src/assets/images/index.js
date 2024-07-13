689889907956aac293e3f1313d79cbca3fb49520
Dave <sololadavid4@gmail.com>
2024-07-13 19:18:11 +0100

    jb image

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,7 +1,7 @@
 import headerLogo from './header-logo.svg'
 import job1 from './job1.png'
 import job2 from './job2.jpg'
-import job3 from './job3.png'
+import job3 from './job3.jpg'
 import background from './background1.jpg'
 import pxfuel from './pxfuel.jpg'
 export {
