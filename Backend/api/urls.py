a640d684cd5f5dc201743444723ab98dafee9425
Dave <sololadavid4@gmail.com>
2024-07-16 21:59:38 +0100

    remove prep work

--- Backend/api/urls.py
+++ Backend/api/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 
-
-
 urlpatterns = [
     path('signup/', views.signup, name='sign up rout'),
     path('login/', views.login, name='login'),
-    path('account', views.account,  name='account'),
     path('protectedview/', views.protectedView, name='protect'),
     path('token/refresh', TokenRefreshView.as_view(), name='token refresh'),
     path('token/access', TokenObtainPairView.as_view(), name='token access')
