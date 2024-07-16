92f8ab67a27c8448eaba3f78ba1cea10c8ce0d4d
Dave <sololadavid4@gmail.com>
2024-07-16 23:27:40 +0100

    states for job

--- src/authcontext/inputContext.jsx
+++ src/authcontext/inputContext.jsx
@@ -5,9 +5,19 @@ export const InputContext = createContext();
 
 const InputProvider = ({ children }) => {
   const [userData, setUserData] = useState([]);
-
+  const [jobDetail, setJobDetail] = useState([]);
+  const [jobDetailsOpen, setJobDetailsOpen] = useState(false);
   return (
-    <InputContext.Provider value={{ userData, setUserData }}>
+    <InputContext.Provider
+      value={{
+        userData,
+        setUserData,
+        jobDetail,
+        setJobDetail,
+        jobDetailsOpen,
+        setJobDetailsOpen,
+      }}
+    >
       {children}
     </InputContext.Provider>
   );
