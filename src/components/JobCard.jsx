10990d8e6311cce6b04abe6eb0f7340b73f610d0
Dave <sololadavid4@gmail.com>
2024-07-30 16:18:10 +0100

    remove static data and replace with loading animation

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -10,6 +10,7 @@ function JobCard() {
     jobDetail,
     setJobDetailsOpen,
     jobDetailsOpen,
+    isLoading,
   } = useContext(InputContext);
 
   const truncateText = (text, limit) => {
@@ -32,181 +33,83 @@ function JobCard() {
   };
   return (
     <div className="">
-      {userData.length > 0 ? (
-        userData.map((job, index) => (
-          <div key={index} className="py-2">
-            {/* it starts here */}
-            <div
-              onClick={() => jobFunction(index)}
-              className="  max-w-4xl p-6 flex flex-row gap-4  rounded-lg border border-gray-300 bg-white"
-            >
-              {/* image */}
-              <div>
-                {job.employer_logo ? (
-                  <img
-                    src={job.employer_logo}
-                    alt="job image"
-                    className=" lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-12  rounded-full"
-                  />
-                ) : (
-                  <img
-                    src={job3}
-                    alt="job image"
-                    className=" lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-28 rounded-full"
-                  />
-                )}
-              </div>
-              <div>
-                <div className="flex space-x-4">
-                  <div className="flex">
-                    <h1 className="mb-2 text-xl font-semibold text-gray-900 hover:underline">
-                      {job.job_title || "Senior Backend Engineer-Payments"}
-                    </h1>
+      {userData.length > 0
+        ? userData.map((job, index) => (
+            <div key={index} className="py-2">
+              {/* it starts here */}
+              <div
+                onClick={() => jobFunction(index)}
+                className="  max-w-4xl p-6 flex flex-row gap-4  rounded-lg border border-gray-300 bg-white"
+              >
+                {/* image */}
+                <div>
+                  {job.employer_logo ? (
+                    <img
+                      src={job.employer_logo}
+                      alt="job image"
+                      className=" lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-12  rounded-full"
+                    />
+                  ) : (
+                    <img
+                      src={job3}
+                      alt="job image"
+                      className=" lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-28 rounded-full"
+                    />
+                  )}
+                </div>
+                <div>
+                  <div className="flex space-x-4">
+                    <div className="flex">
+                      <h1 className="mb-2 text-xl font-semibold text-gray-900 hover:underline">
+                        {job.job_title || "Senior Backend Engineer-Payments"}
+                      </h1>
+                    </div>
                   </div>
-                </div>
-                <p>{job.employer_name} </p>
+                  <p>{job.employer_name} </p>
 
-                <p>{job.job_city === null ? job.job_country : job.job_city}</p>
-                <br />
-                <div className="">
-                  {job.job_highlights &&
-                    job.job_highlights.Responsibilities?.slice(0, 3).map(
-                      (item, i) => (
-                        <div key={i} className="flex items-center">
-                          <button>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              width="24"
-                              height="24"
-                              viewBox="0 0 24 24"
-                              fill="none"
-                              stroke="currentColor"
-                              strokeWidth="2"
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              className="lucide lucide-dot text-gray-900"
-                            >
-                              <circle cx="12.1" cy="12.1" r="1" />
-                            </svg>
-                          </button>
-                          <p className="font-sans text-gray-600">
-                            {truncateText(item, 120)}
-                          </p>
-                          <br />
-                        </div>
-                      )
-                    )}
-                </div>
-              </div>
-            </div>
-          </div>
-        ))
-      ) : (
-        <div className="">
-          <div
-            onClick={() => test()}
-            className=" cursor-pointer p-6 flex flex-row gap-4  rounded-lg border border-gray-300 bg-white"
-          >
-            <div>
-              <img
-                src={job3}
-                alt="job image"
-                className=" h-12 w-13 rounded-full"
-              />
-            </div>
-            <div>
-              <div className="flex space-x-4">
-                <div className="flex">
-                  <h1 className="mb-2 text-xl font-semibold text-gray-900 hover:underline">
-                    Senior Backend Engineer-Payments
-                  </h1>
-                </div>
-              </div>
-              <p>Gurugeeks Royalty Limited </p>
-
-              <p>Ikeja</p>
-              <br />
-              <div className="px-2">
-                <div className="flex items-center space-x-4">
-                  <button>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6 text-lg text-gray-600"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                      />
-                    </svg>
-                  </button>
-                  <p className="font-sans text-gray-600">
-                    The ideal candidate will have a unique blend of technical
-                    expertise, insatiable curiosity
+                  <p>
+                    {job.job_city === null ? job.job_country : job.job_city}
                   </p>
                   <br />
-                </div>
-                <div className="flex items-center mt-2 space-x-4">
-                  <button>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      className="w-6 h-6 text-gray-600"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                      />
-                    </svg>
-                  </button>
-                  <p className="font-sans text-gray-600">
-                    Experience with JavaScript, Amazon Web Service(AWS), Git, or
-                    MongoDB is helpful to... this will work is ass out
-                  </p>
+                  <div className="">
+                    {job.job_highlights &&
+                      job.job_highlights.Responsibilities?.slice(0, 3).map(
+                        (item, i) => (
+                          <div key={i} className="flex items-center">
+                            <button>
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-dot text-gray-900"
+                              >
+                                <circle cx="12.1" cy="12.1" r="1" />
+                              </svg>
+                            </button>
+                            <p className="font-sans text-gray-600">
+                              {truncateText(item, 120)}
+                            </p>
+                            <br />
+                          </div>
+                        )
+                      )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          ))
+        : isLoading && (
+            <div className="flex items-center justify-center h-screen    ">
+              <div className="loader"></div>
+            </div>
+          )}
 
-      {/* <div className=" absolute h-screen overflow-y-auto items-center flex justify-center ">
-        {jobDetailsOpen && <JobDetail />}
-      </div> */}
-      {/* 
-        Header card
-        -job title
-        - { componany website link with name of company on it ?  company website  && location }
-        Apply now button
-        
-      
-        # full job Desrciption
-        -Job title 
-        -location 
-        -job description (brief)
-
-        # key Responsibility:
-         -Design this and that
-
-         #reuired skill and requirement :
-          -Bachelor’s degree in Computer Science, Engineering, or a related field (or equivalent practical experience).
-
-         # Preferred skills 
-         -Experience with cloud platforms (e.g., AWS, Azure, Google Cloud).
-
-         #Job type: Hybrid
-         
-
-      
-      */}
       {jobDetailsOpen && <JobDetail />}
     </div>
   );
