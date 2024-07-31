5355cac3b5f2c5a9aed767f93f9655a5c9103982
Dave <sololadavid4@gmail.com>
2024-07-31 19:43:50 +0100

    add new svg

--- public/index.js
+++ public/index.js
@@ -1,10 +1,12 @@
 import locksvg from './lock.svg'
 import check from './check.svg'
 import recBox from './recBox.svg'
+import  briefcase  from './briefcase.svg'
 
 
 export {
     locksvg,
     check,
     recBox,
+    briefcase
 }
