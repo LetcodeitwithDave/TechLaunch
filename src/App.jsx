8d7669f72972717377a38e1ee4e064d775c48223
Dave <sololadavid4@gmail.com>
2024-08-01 17:02:56 +0100

    reorganize my code and include jobDetail context

--- src/App.jsx
+++ src/App.jsx
@@ -19,72 +19,52 @@ import Unauthourized from "./authcontext/Unauthourized";
 import JobDetailsPage from "../pages/JobDetailsPage";
 import Know from "../public/Know";
 import { InputProvider } from "./authcontext/inputContext";
+import { JobDetailProvider } from "./authcontext/JobDetailContext";
+
 function App() {
   return (
     <AuthProvider>
       <InputProvider>
-        <Router>
-          <Routes>
-            <Route
-              element={<Unauthourized children={<Login />} />}
-              path="/login"
-            />
+        <JobDetailProvider>
+          <Router>
+            <Routes>
+              <Route
+                element={<Unauthourized children={<Login />} />}
+                path="/login"
+              />
 
-            <Route
-              element={<Unauthourized children={<SignUp />} />}
-              path="/signup"
-            />
+              <Route
+                element={<Unauthourized children={<SignUp />} />}
+                path="/signup"
+              />
 
-            <Route
-              element={<Unauthourized children={<LandingPage />} />}
-              path="/"
-            />
+              <Route
+                element={<Unauthourized children={<LandingPage />} />}
+                path="/"
+              />
 
-            <Route
-              element={<ProtectedRoute children={<Know />} />}
-              path="/know"
-            />
+              <Route
+                element={<ProtectedRoute children={<Know />} />}
+                path="/know"
+              />
 
-            <Route
-              element={<ProtectedRoute children={<Main />} />}
-              path="/home"
-            />
+              <Route
+                element={<ProtectedRoute children={<Main />} />}
+                path="/home"
+              />
 
-            <Route
-              element={<ProtectedRoute children={<JobDetailsPage />} />}
-              path="/jobpage"
-            />
-          </Routes>
-        </Router>
+              <Route
+                element={<ProtectedRoute children={<JobDetailsPage />} />}
+                path="/jobpage/:jobId"
+              />
+            </Routes>
+          </Router>
+        </JobDetailProvider>
       </InputProvider>
     </AuthProvider>
     // </div>
   );
 }
-
-// const AppRoutes = () => {
-//   const navigate = useNavigate();
-//   const { isAuthenticated } = useAuth();
-//   return (
-//     <Routes>
-//       <Route path="/home" element={<ProtectedRoute />} />
-//       <Route
-//         path="/"
-//         element={isAuthenticated ? navigate("/home") : <LandingPage />}
-//       />
-//       <Route
-//         path="/signup"
-//         element={isAuthenticated ? navigate("/home") : <Signup />}
-//       />
-//       <Route
-//         element={isAuthenticated ? navigate("/home") : <Login />}
-//         path="/login"
-//       />
-
-//       <Route path="/test" element={<Account />} />
-//     </Routes>
-//   );
-// };
 
 export default App;
 library.add(far, fas);
