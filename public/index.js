7282dddfcbeb317e9de56be3400ef43343ee8766
Dave <sololadavid4@gmail.com>
2024-08-02 22:09:40 +0100

    update

--- public/index.js
+++ public/index.js
@@ -2,11 +2,12 @@ import locksvg from './lock.svg'
 import check from './check.svg'
 import recBox from './recBox.svg'
 import  briefcase  from './briefcase.svg'
-
+import chevronback from './chevronback.svg'
 
 export {
     locksvg,
     check,
     recBox,
-    briefcase
+    briefcase,
+    chevronback,
 }
