7ed03190343ede19bdf9bd82280bec009d3373d6
Dave <sololadavid4@gmail.com>
2024-08-02 23:42:09 +0100

    updated

--- src/components/JobDetailsPage.jsx
+++ src/components/JobDetailsPage.jsx
@@ -3,74 +3,87 @@ import { LandingHeader } from "../landingpage/components";
 import { JobContext } from "../authcontext/JobDetailContext";
 import { job3, jobBackgroundImage } from "../assets/images";
 import BackButton from "./BackButton";
+import { nocompanyimage } from "../assets/images";
 
 function JobDetailsPage() {
   const { jobInfo } = useContext(JobContext);
 
-  return (
-    <div className=" bg-test mx-6">
-      <LandingHeader />
-      <div className=" mt-8">
-        <BackButton />
-      </div>
-      <div className=" mt-5 border bg-white max-w-4xl ">
-        <div className=" p-2 rounded-t-lg">
-          <div>
-            <img src={jobBackgroundImage} className=" rounded-t-lg" alt="" />
-          </div>
+  const timePosted = (timestamp) => {
+    const givenDateInMs = timestamp * 1000;
+    const now = Date.now();
+    const diffInMs = now - givenDateInMs;
+    const diffInHours = diffInMs / (1000 * 60 * 60);
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
 
-          {jobInfo.employer_logo ? (
-            <img
-              src={jobInfo.employer_logo}
-              alt="job image"
-              className="lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-12 rounded-full"
-            />
-          ) : (
-            <img
-              src={job3}
-              alt="job image"
-              className="lg:h-12 lg:w-14 md:h-12 md:w-16 h-12 w-28 rounded-full"
-            />
-          )}
-          <h1 className="mb-2 text-2xl font-robotoMedium  text-textColor">
-            {jobInfo.job_title}
-          </h1>
-          <div className="font-robotoRegular text-textColor flex space-x-2">
-            <p>
-              <a
-                className={
-                  jobInfo.employer_website &&
-                  "underline underline-offset-4 decoration-gray-600"
-                }
-                href={jobInfo.employer_website && jobInfo.employer_website}
-              >
-                {jobInfo.employer_name}
-              </a>
-            </p>
-            <p>
-              |{" "}
-              {jobInfo.job_city === null
-                ? jobInfo.job_country
-                : jobInfo.job_city}
-            </p>
-          </div>
-          <button className="transition duration-300 hover:bg-blue-600 px-4 py-2 rounded-md mt-4 bg-green-400 text-white font-robotoMedium">
-            <a href={jobInfo.apply_options.map((apply) => apply.apply_link)}>
-              Apply Now
-            </a>
-          </button>
-          <div className="mt-0 rounded-lg">
-            <h1 className="text-xl font-robotoMedium text-textColor">
-              Full job description
-            </h1>
-            <p className="text-md mt-6">Job Title: {jobInfo.job_title}</p>
-            <p className="text-md mt-4">
-              Location: {jobInfo.job_city || jobInfo.job_country}
-            </p>
-            <div
-              className="mt-8"
-              dangerouslySetInnerHTML={{ __html: jobInfo.job_description }}
-            />
+    if (diffInHours < 24) {
+      const hours = Math.floor(diffInHours);
+      return `${hours} hours ago`;
+    } else {
+      const days = Math.floor(diffInDays);
+      return `${days} days ago`;
+    }
+  };
+
+  return (
+    <div className=" bg-test ">
+      <LandingHeader />
+      <div className="mx-6 ">
+        <div className=" mt-8">
+          <BackButton />
+        </div>
+        <div className=" mt-8 p-4  bg-white rounded-md max-w-4xl  ">
+          <div className="  ">
+            <div className="relative">
+              <img
+                src={jobBackgroundImage}
+                className="rounded-t-lg w-full "
+                alt=""
+              />
+              <div className=" mx-4">
+                <div className="flex-shrink-0 w-20 h-20 p-1  top-9 -mt-9 bg-heroBackground rounded-md">
+                  <img
+                    src={jobInfo.employer_logo || nocompanyimage}
+                    alt="job image"
+                    className="w-full h-full object-cover rounded-md "
+                  />
+                </div>
+                <div className=" flex  justify-between items-center mt-8">
+                  <h1 className="mb-2 text-xl md:text-3xl font-robotoMedium  text-textColor">
+                    {jobInfo.job_title}
+                  </h1>
+
+                  <button className="transition duration-300 hover:bg-blue-600 px-4 py-2 rounded-md mt-4 bg-green-400 text-white font-robotoMedium">
+                    <a
+                      href={jobInfo.apply_options.map(
+                        (apply) => apply.apply_link
+                      )}
+                    >
+                      Apply Now
+                    </a>
+                  </button>
+                </div>
+
+                <div className="flex space-x-1 mt-0 text-gray-600 font-robotoRegular text-sm">
+                  <p>{jobInfo.employer_name}</p>
+                  {jobInfo.job_country && <span>•</span>}
+                  <p>
+                    {jobInfo.job_country && jobInfo.job_city
+                      ? `${jobInfo.job_city}, ${jobInfo.job_country}`
+                      : jobInfo.job_country}
+                  </p>
+                  {jobInfo.job_posted_at_timestamp && (
+                    <span>• {timePosted(jobInfo.job_posted_at_timestamp)}</span>
+                  )}
+                </div>
+
+                <div
+                  className="mt-8"
+                  dangerouslySetInnerHTML={{
+                    __html: jobInfo.job_description,
+                  }}
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
