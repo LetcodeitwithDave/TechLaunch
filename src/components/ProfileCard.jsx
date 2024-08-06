32d7e3d55cab9e6767212a384b2aa25d234d0bad
Dave <sololadavid4@gmail.com>
2024-08-06 17:37:45 +0100

    updage code

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function ProfileCard() {
   return (
-    <div>
+    <div className=" w-64 px-5 h-96 overflow-y-auto sticky top-0  bg-white rounded-xl">
       <h1>Profile</h1>
     </div>
   );
