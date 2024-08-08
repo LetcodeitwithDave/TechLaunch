fc2cd6804b2c18a8226a6a81f3262ad4c06d3c85
Dave <sololadavid4@gmail.com>
2024-08-08 22:22:01 +0100

    loading animation

---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Loader() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center ">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
